8cda88a4c1006f723db07fc29abbbb2ac9b0e3e1
DevMarcoValeri <info@marcovaleri.net>
2020-05-23 20:22:33 +0100

    Finished HomePage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,6 @@
 import React from 'react';
 
-import Header from './components/header/header.component';
-import Main from './components/main/main.component';
-import Footer from './components/footer/footer.components';
-
-// Import images
-import blackMoon from './images/blackMoon.jpg';
-import moonDarkness from './images/moonDarkness.jpg';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
 
@@ -14,11 +8,7 @@ class App extends React.Component {
   
   render() {
     return (
-      <div>
-        <Header imageUrl={blackMoon} />
-        <Main imageUrlOne={moonDarkness} imageUrlTwo={blackMoon} />
-        <Footer />
-      </div>
+      <HomePage />
     )
   }
 
