6b2f125c5dd454b761e608baa6f57e3648f384fb
DevMarcoValeri <info@marcovaleri.net>
2020-05-27 20:01:47 +0100

    Updates

---
+++ precache-manifest.d12add2c3a8daba5f88c722e552c0781.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9260f5065fee784e5ea818a5b3741f4",
+    "url": "/nasa-gallery/index.html"
+  },
+  {
+    "revision": "7562ed8e607d104f55d4",
+    "url": "/nasa-gallery/static/css/main.58d6cd80.chunk.css"
+  },
+  {
+    "revision": "e2b03062317dff7cfb88",
+    "url": "/nasa-gallery/static/js/2.0cde5f3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nasa-gallery/static/js/2.0cde5f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7562ed8e607d104f55d4",
+    "url": "/nasa-gallery/static/js/main.87fa2441.chunk.js"
+  },
+  {
+    "revision": "eb9301e4b42e17cb03fe",
+    "url": "/nasa-gallery/static/js/runtime-main.cacad89a.js"
+  },
+  {
+    "revision": "59273deafa4ca6f619db7442ce779deb",
+    "url": "/nasa-gallery/static/media/blackMoon.59273dea.jpg"
+  },
+  {
+    "revision": "2f9a27ca02a74c01dbc6cc44041fcb73",
+    "url": "/nasa-gallery/static/media/moonDarkness.2f9a27ca.jpg"
+  }
+]);
